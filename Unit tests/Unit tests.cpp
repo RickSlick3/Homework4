51f246e8627c5c0292ae8326df79f69b8d610fd3
AndrewMcCrary <ajmccrary03@gmail.com>
2022-04-11 17:54:44 -0400

    Add testing project

---
+++ Unit tests/Unit tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Unittests
+{
+	TEST_CLASS(Unittests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
